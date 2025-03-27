361953cc41a13372fbb62089f4cd9117e244b42f
Moxh <moxhsharma878@gmail.com>
2025-03-27 15:29:50 +0530

    some text print

--- right_angle_triangle.java
+++ right_angle_triangle.java
@@ -1,7 +1,7 @@
 public class right_angle_triangle {
     public static void main(String[] args) {
         int rows = 5; // Number of rows for the triangle
-
+System.out.println("this is ur triangle");
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
